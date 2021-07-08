8a6180e328d2463038ba848e19ded502313af403
p.bailey <pabailey123@gmail.com>
2021-07-08 18:29:28 -0400

    Removed unnecessary test from CoinValidatorTest -pbai

--- VendingMachineSimulation/testSrc/controlTest/CoinValidatorTest.java
+++ VendingMachineSimulation/testSrc/controlTest/CoinValidatorTest.java
@@ -7,11 +7,6 @@ import control.CoinValidatorInterface;
 
 public class CoinValidatorTest extends TestCase {
 
-	public void testImplementsCoinValidatorInterface() throws Exception {
-		assertEquals(1, CoinValidator.class.getInterfaces().length);
-		assertEquals(CoinValidatorInterface.class, CoinValidator.class.getInterfaces()[0]);
-	}
-
 	public void testCoinIsValidReturnsTrueWhenValidCoinIsInserted() throws Exception {
 		CoinValidatorInterface validator = new CoinValidator();
 		assertTrue(validator.coinIsValid(InsertedCoinPiece.NICKEL));
